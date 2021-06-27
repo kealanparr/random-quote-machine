0b5542fc2d4368facce49d7b99286214d280809c
kealan-parr <kea_parr@hotmail.co.uk>
2021-06-27 18:44:28 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Quote from './Components/Quote.js'
+
+function App() {
+  return (
+   <Quote />
+  );
+}
+
+export default App;
---
+++ src/Components/Quote.js
@@ -0,0 +1,7 @@
+const Quote = () => {
+	return (
+		<h1>Basic wire up :) </h1>
+	)
+};
+
+export default Quote;
