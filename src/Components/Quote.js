c5dd056444619431be9b078351163255cfe7fb1d
kealan-parr <kea_parr@hotmail.co.uk>
2021-07-13 00:34:02 +0100

    Turn JSON into JS, update README.md and

--- src/Components/Quote.js
+++ src/Components/Quote.js
@@ -1,4 +1,4 @@
-import State from '../state.json'
+import State from '../state'
 import React, { useState } from 'react';
 
 const QuoteModal = () => {
@@ -7,14 +7,16 @@ const QuoteModal = () => {
 
 	const changeColor = (color) => {
 		document.body.style.backgroundColor = color;
+		document.getElementById("text").style.backgroundColor = color;
+		document.getElementById("author").style.backgroundColor = color;
 	}
 	
 	return (
 		State &&
 		<div>
-				<div id="text">{State.data[index].quote}</div>
-				<div id="author">{State.data[index].author}</div>
-				<InfoPanel index={index} setIndex={setIndex} changeColor={changeColor} />
+			<div id="text">{State[index].quote}</div>
+			<div id="author">{State[index].author}</div>
+			<InfoPanel index={index} setIndex={setIndex} changeColor={changeColor} />
 		</div>
 	)
 };
@@ -23,17 +25,17 @@ const InfoPanel = ({index, setIndex, changeColor}) => {
 	return (
 		State &&
 		<div>
-				<a id="tweet-quote" href="https://twitter.com/intent/tweet">Tweet</a>
-				<div onClick={() => {
-					debugger;
-					const newIndex = index + 1;
-					if (newIndex === State.data.length) {
-						setIndex(0);
-					} else {
-						setIndex(index + 1);
-					}
-					changeColor(State.data[index].color);
-				}} id="new-quote"></div>
+			<a id="tweet-quote" href="https://twitter.com/intent/tweet">Tweet</a>
+			<div onClick={() => {
+				debugger;
+				const newIndex = index + 1;
+				if (newIndex === State.length) {
+					setIndex(0);
+				} else {
+					setIndex(index + 1);
+				}
+				changeColor(State[index].color);
+			}} id="new-quote">New Quote</div>
 		</div>
 	)
 }
---
+++ src/state.js
@@ -0,0 +1,34 @@
+const state = [
+	{
+		"author": "Elizabeth Gilbert",
+		"quote": "What do you love doing so much that the words failure and success essentially become irrelevant?",
+		"color": "#D4E09B"
+	},
+	{
+		"author": "Mark Twain",
+		"quote": "Twenty years from now you will be more disappointed by the things you didn't do, than by the ones you did do, so throw bowlines, sail away from the safe harbor, catch the trade winds in your sail. Explore, dream, discover",
+		"color": "#F6F4D2"
+	},
+	{
+		"author": "Randy Pausch",
+		"quote": "Work hard. I got tenure a year early. Junior faculty members used to say to me: 'Wow, what's your secret?' I said: 'It's pretty simple. Call me any Friday night in my office at 10 o'clock, and I'll tell you.'",
+		"color": "#CBDFBD"
+	},
+	{
+		"author": "Socrates",
+		"quote": "The unexamined life is not worth living",
+		"color": "#F19C79"
+	},
+	{
+		"author": "Ray Bradbury",
+		"quote": "If you drown, at least die knowing you were headed to shore",
+		"color": "#FFBF80"
+	},
+	{
+		"author": "Charles Spurgeon",
+		"quote": "Anxiety does not empty tomorrow of its sorrows, only today of its strength",
+		"color": "#A44A3F"
+	}
+]
+
+export default state;
