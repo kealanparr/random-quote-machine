d52641305e7872e53c670a44a126832ac7a3b674
kealan-parr <kea_parr@hotmail.co.uk>
2021-07-12 19:26:45 +0100

    Wire up for the actual functionality and also to work on transition - finally to pass tests

--- src/Components/Quote.js
+++ src/Components/Quote.js
@@ -2,27 +2,37 @@ import State from '../state.json'
 import React, { useState } from 'react';
 
 const QuoteModal = () => {
+
+	const [index, setIndex] = useState(0);
+
+	const changeColor = (color) => {
+		document.body.style.backgroundColor = color;
+	}
+	
 	return (
-		<div>
-				{ /* 	<i className="fas fa-quote-left"></i> */ }
-				<div id="text">{State.data[State.index].quote}</div>
-				<div id="author">{State.data[State.index].author}</div>
-				<InfoPanel />
-		</div>
+			State &&
+			<div>
+					<div id="text">{State.data[index].quote}</div>
+					<div id="author">{State.data[index].author}</div>	
+					<InfoPanel index={index} setIndex={setIndex} changeColor={changeColor} />
+			</div>
 	)
 };
 
-const InfoPanel = () => {
-	
-		const [index, nextQuote] = useState(0);
-
+const InfoPanel = ({index, setIndex, changeColor}) => {
 	return (
 		<div>
-			<a href="twitter.com/intent/tweet">Tweet</a>
-			<div onClick={() => {
-				debugger;
-				nextQuote(index + 1);
-			}} id="new-quote"></div>
+				<a id="tweet-quote" href="twitter.com/intent/tweet">Tweet</a>
+				<div onClick={() => {
+					debugger;
+					const newIndex = index + 1;
+					if (newIndex === State.data.length) {
+						setIndex(0);
+					} else {
+						setIndex(index + 1);
+					}
+					changeColor(State.data[index].color);
+				}} id="new-quote"></div>
 		</div>
 	)
 }
